d2b63d4957be531f49e11d5c90e9bf6dd4f7b6a0
Tim De Baets <tdebaets@gmail.com>
2016-02-20 22:58:10 +0100

    Add setuprepo script to set default pull behaviour to rebase with preserve

---
+++ postclone.bat
@@ -0,0 +1,36 @@
+@echo off
+
+rem **************************************************************************
+rem *
+rem * Licensed under the Apache License, Version 2.0 (the "License");
+rem * you may not use this file except in compliance with the License.
+rem * You may obtain a copy of the License at
+rem *
+rem *     http://www.apache.org/licenses/LICENSE-2.0
+rem *
+rem * Unless required by applicable law or agreed to in writing, software
+rem * distributed under the License is distributed on an "AS IS" BASIS,
+rem * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem * See the License for the specific language governing permissions and
+rem * limitations under the License.
+rem *
+rem **************************************************************************
+rem *
+rem * One-time script to run after cloning the repository
+rem *
+rem **************************************************************************
+
+setlocal
+
+call .\Scripts\setuprepo.bat
+if errorlevel 1 goto failed
+
+echo Success!
+goto exit
+
+:failed
+echo *** FAILED ***
+:failed2
+exit /b 1
+
+:exit
