16e00946a1dd6eae604055044862730fa8128bdd
Tim De Baets <tdebaets@gmail.com>
2016-07-09 21:54:01 +0200

    Add script to manually update the common submodule

---
+++ Scripts/updatecommon.bat
@@ -0,0 +1,42 @@
+@echo off
+
+rem **************************************************************************
+rem *
+rem * Licensed under the Apache License, Version 2.0 (the "License");
+rem * you may not use this file except in compliance with the License.
+rem * You may obtain a copy of the License at
+rem *
+rem *     http://www.apache.org/licenses/LICENSE-2.0
+rem *
+rem * Unless required by applicable law or agreed to in writing, software
+rem * distributed under the License is distributed on an "AS IS" BASIS,
+rem * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem * See the License for the specific language governing permissions and
+rem * limitations under the License.
+rem *
+rem **************************************************************************
+rem *
+rem * Script to manually update the common submodule in a repository
+rem *
+rem **************************************************************************
+
+setlocal
+
+if not exist common (
+    echo The common submodule wasn't found.
+    goto exit
+)
+
+echo Updating common submodule...
+git submodule update --remote common
+if errorlevel 1 goto failed
+
+echo Success!
+goto exit
+
+:failed
+echo *** FAILED ***
+:failed2
+exit /b 1
+
+:exit
